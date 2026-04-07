b62804e1445c2e1d0826fbdd26ee8c3e1c4378cb
Yailyn Esperanza Santana Rojas <20251104@itla.edu.do>
2026-04-07 14:51:50 -0400

    exceptions y web

---
+++ GourmetGo.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using GourmetGo.Application.Base;
+using System.Net;
+using System.Text.Json;
+
+namespace GourmetGo.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger< ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invokeasync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error critico interceptado: {Mensaje}", ex.Message);
+
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = Result<string>.Fail("Ha ocurrido un error interno en el servidor. El equipo técnico ha sido notificado ");
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(response, options);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+
+
+ 
+}
